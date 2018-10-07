cbabb7bbbc5df4d426dbec14b8df6995d9a67600
Ajay Simha Subraveti Ranganatha <ajaysimhasr@gmail.com>
2018-10-07 12:00:27 -0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -7,15 +7,14 @@ Created on Sat Oct  6 16:38:23 2018
 """
 
 from twilio.rest import Client
-from receive_sms import sms_reply
 
-account_sid = 'AC98f9c9f7398d39e60ce1fb59dd3f8b2b'
-auth_token = 'bb0a8423dacc34ca395d8f3be08bc61c'
+account_sid = 'Axxxxxxxxxxxxxxxxx'
+auth_token = 'bbzzzzzzzzzzzzzzzzz'
 
 client = Client(account_sid, auth_token)
 
 client.messages.create(
-        to = '+12013493160',
-        from_ = '+19292039742',
+        to = '+12222222222',
+        from_ = '+1234567894',
         body = 'This is my first twilio text message'
         )
